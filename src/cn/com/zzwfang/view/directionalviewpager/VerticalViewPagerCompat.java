7e71661074d6eb55f604c0163c2916a95a833290
lzd <lzd@doooer.com>
2015-12-18 18:48:49 +0800

    moify pager

---
+++ src/cn/com/zzwfang/view/directionalviewpager/VerticalViewPagerCompat.java
@@ -0,0 +1,15 @@
+package cn.com.zzwfang.view.directionalviewpager;
+
+import android.database.DataSetObserver;
+import android.support.v4.view.PagerAdapter;
+
+public final class VerticalViewPagerCompat {
+    private VerticalViewPagerCompat() {}
+
+//    public interface DataSetObserver extends android.database.DataSetObserver.DataSetObserver {}
+
+//    public static void setDataSetObserver(PagerAdapter adapter, DataSetObserver observer) {
+////        adapter.setDataSetObserver(observer);
+//        adapter.registerDataSetObserver(observer);
+//    }
+}
